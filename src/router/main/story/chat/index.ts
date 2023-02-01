2cdfbda145ebf492cd3acc5fbbc39f32e5611918
kabuto <31604422@qq.com>
2023-02-01 21:17:19 +0800

    feature(custom): 增加meta标题

--- src/router/main/story/chat/index.ts
+++ src/router/main/story/chat/index.ts
@@ -1,4 +1,7 @@
 export default {
   path: '/main/story/chat',
   component: () => import('@/views/main/story/chat/index.vue'),
+  meta: {
+    title: '你的故事',
+  },
 }
--- src/router/main/story/list/index.ts
+++ src/router/main/story/list/index.ts
@@ -1,4 +1,7 @@
 export default {
   path: '/main/story/list',
   component: () => import('@/views/main/story/list/index.vue'),
+  meta: {
+    title: '故事列表',
+  },
 }
